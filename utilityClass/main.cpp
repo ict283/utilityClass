216e8b79d1fb9f0a64d5d71346a256f47d5d7ff2
ict283 <glennlum@icloud.com>
2021-03-21 02:17:13 +0800

    Initial Commit

---
+++ utilityClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  utilityClass
+//
+//  Created by Glenn Lum on 21/3/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
