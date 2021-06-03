921ee5f3577557432a39f2e50c64df1d1b311b71
ict283 <glennlum@icloud.com>
2021-06-03 21:33:41 +0800

    Start code review

--- utilityClass/main.cpp
+++ utilityClass/main.cpp
@@ -1,4 +1,4 @@
-// For review purposes only! Does not represent the actual solution!
+// For review purposes only! Does not represent the actual solution!!
 
 #include <stdio.h>
 #include <iostream>
