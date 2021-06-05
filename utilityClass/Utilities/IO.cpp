41ce60644376f5a295f5947395c42a56ed721ab3
ict283 <glennlum@icloud.com>
2021-06-05 13:12:09 +0800

    Define and implement an IO helper class

--- utilityClass/Utilities/IO.cpp
+++ utilityClass/Utilities/IO.cpp
@@ -1,3 +1,82 @@
 // For review purposes only! Does not represent the actual solution!
 
 #include "IO.h"
+
+IO::IO()
+{
+    cout<<"IO Ready!"<<endl;
+}
+
+IO::~IO()
+{
+    cout<<"IO Shut Down!"<<endl;
+}
+
+/* file readers */
+
+bool IO::personReader (string file, Person& person){
+    
+    bool result = false;
+    ifstream infile (file);
+    
+    if(infile.is_open())
+    {
+        string str;
+        int num;
+        getline(infile, str,',');
+        infile >> num;
+        person.setName(str);
+        person.setAge(num);
+        infile.close();
+        result=true;
+    }
+    return result;
+}
+
+
+bool IO::primeReader (string file, Prime& prime){
+    
+        bool result = false;
+        ifstream infile (file);
+    
+        if(infile.is_open())
+        {
+            int num;
+            infile >> num;
+            prime.setPrime(num);
+            infile.close();
+            result=true;
+        }
+        return result;
+}
+
+/* file writers */
+
+bool IO::personWriter (string file, Person& person){
+        
+        bool result = false;
+        ofstream outfile (file);
+    
+        if(outfile.is_open())
+        {
+            outfile<<"personWriter : "<<person.getName()<<person.getAge();
+            outfile.close();
+            result=true;
+        }
+        return result;
+}
+
+
+bool IO::primeWriter (string file, Prime& prime){
+    
+        bool result = false;
+        ofstream outfile (file);
+    
+        if(outfile.is_open())
+        {
+            outfile<<"primeWriter : "<<prime.getPrime();
+            outfile.close();
+            result = true;
+        }
+        return result;
+}
--- utilityClass/Utilities/IO.h
+++ utilityClass/Utilities/IO.h
@@ -3,9 +3,30 @@
 #ifndef IO_h
 #define IO_h
 
+#include<stdio.h>
+#include<fstream>
+#include "../Classes/Person.h"
+#include "../Classes/Prime.h"
+
+using namespace std;
+
 class IO
 {
-
+public:
+    
+    IO();
+    
+    ~IO();
+    
+    /* file readers */
+    bool personReader (string file, Person& person);
+    
+    bool primeReader (string file, Prime& prime);
+    
+    /* file writers */
+    bool personWriter (string file, Person& person);
+    
+    bool primeWriter (string file, Prime& prime);
 
 };
 
