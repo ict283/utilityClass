8fb207002b8d3eef7fd208185c1cd3e5c8373940
ict283 <glennlum@icloud.com>
2021-06-05 13:19:03 +0800

    Refactor main program to use IO helper

--- utilityClass/main.cpp
+++ utilityClass/main.cpp
@@ -2,8 +2,8 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <fstream> //filestream
 #include <string>
+#include "Utilities/IO.h"
 #include "Classes/Person.h"
 #include "Classes/Prime.h"
 
@@ -11,58 +11,17 @@ using namespace std;
 
 int main(void)
 {
-        //File IO - Person Class
-
+        IO util;
+    
+        /* File IO - Person Class */
         Person person;
-        ifstream infile_1 ("person_in.txt");
+        util.personReader("person_in.txt",person);
+        util.personWriter("person_out.txt",person);
 
-        if(infile_1.is_open())
-        {
-                infile_1>>person;
-                infile_1.close();
-        }
-        else
-        {
-                cout<<"Error reading file!"<<endl;
-        }
-
-        ofstream outfile_1 ("person_out.txt");
-
-        if(outfile_1.is_open())
-        {
-                outfile_1<<person;
-                outfile_1.close();
-        }
-        else
-        {
-                cout<<"Error writing file!"<<endl;
-        }
-
-        //File IO - Prime Class
-
+        /* File IO - Prime Class */
         Prime prime;
-        ifstream infile_2 ("prime_in.txt");
-
-        if(infile_2.is_open())
-        {
-                infile_2>>prime;
-        }
-        else
-        {
-                cout<<"Error reading file!"<<endl;
-        }
-
-        ofstream outfile_2 ("prime_out.txt");
-
-        if(outfile_2.is_open())
-        {
-                outfile_2<<prime;
-                outfile_2.close();
-        }
-        else
-        {
-                cout<<"Error writing file!"<<endl;
-        }
+        util.primeReader("prime_in.txt",prime);
+        util.primeWriter("prime_out.txt",prime);
 
         return 0;
 }
